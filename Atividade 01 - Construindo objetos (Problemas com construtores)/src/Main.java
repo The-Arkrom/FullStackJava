0db43918e9ea5cedb5701d8799bbf8658607a12e
Luis Castro <luisr.contato@gmail.com>
2025-09-04 10:38:46 -0300

    Add files via upload

---
+++ Atividade 01 - Construindo objetos (Problemas com construtores)/src/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world");
+
+        System.out.println();
+
+        Produto produto = new Produto("Produto");
+        produto.setNome("Café");
+        produto.setPreco(4.99);
+        System.out.println();
+    }
+}
