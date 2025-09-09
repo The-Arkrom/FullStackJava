66b9df42221820a2fbd8e21469e88975cfd52159
Luis Castro <luisr.contato@gmail.com>
2025-09-09 10:23:15 -0300

    Add files via upload

---
+++ Atividade 02 - Construindo objetos/src/Main.java
@@ -0,0 +1,11 @@
+import Pacote.Pessoa;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world");
+
+        System.out.println();
+
+        Pessoa pessoa = new Pessoa("Rita", 18);
+    }
+}
